1348c0652873a83f0dd1130192557d84a61c5872
unknown <romyguibelondo@gmail.com>
2022-12-06 13:51:10 +0800

    upload ecommerce

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavTop from "./page/NavTop";
+
+
+function App() {
+  return (
+    <div id='page' className="site">
+      <NavTop />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/NavTop.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavTop() {
+  return (
+    <div className='aside site-off desktop-hide'>
+        <div className='off-canvas'>
+            <div className='canvas-head flexitem'>
+                <div className='logo'>
+                    <a href='#'><span className='circle'>GVT</span></a>
+                </div>
+                    <a href='#' className='t-close flexcenter'><span className='ri-close-line'></span></a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NavTop
